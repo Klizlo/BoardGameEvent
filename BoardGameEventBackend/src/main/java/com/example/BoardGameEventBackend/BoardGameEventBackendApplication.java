69a8fb0def8b7d576552bd4f594371aba6e6b8b1
Klizlo <klizlo0111@gmail.com>
2022-11-17 11:51:54 +0100

    Initial commit

---
+++ BoardGameEventBackend/src/main/java/com/example/BoardGameEventBackend/BoardGameEventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoardGameEventBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameEventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameEventBackendApplication.class, args);
+	}
+
+}
---
+++ BoardGameEventBackend/src/test/java/com/example/BoardGameEventBackend/BoardGameEventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BoardGameEventBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameEventBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
